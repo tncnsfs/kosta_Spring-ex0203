6d5315156b057f4ff47d3fdff62b3d6011ab2f8c
tncnsfs@gmail.com <tncnsfs@gmail.com>
2019-02-04 02:25:59 +0900

    paging 이동까지, P313

---
+++ GongZuo/ex02_CSC/190204_ex02/ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -0,0 +1,107 @@
+package org.zerock.mapper;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+
+	@Setter(onMethod_=@Autowired)
+	private BoardMapper mapper;
+	
+	@Test
+	public void testPaging() {
+		
+		Criteria cri = new Criteria();
+		cri.setPageNum(3);
+		cri.setAmount(10);
+		
+		List<BoardVO> list = mapper.getListWithPaging(cri);
+		
+		list.forEach(board -> log.info(board.getBno()));
+	}
+	
+	
+	
+	
+/*	@Test
+	public void testUpdate() {
+		
+		BoardVO board = new BoardVO();
+		board.setBno(2L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("user00");
+		
+		int count = mapper.update(board);
+		log.info("update count:" + count);
+		
+	}*/
+	
+/*	@Test
+	public void testDelete() {
+		
+		log.info("DELETE COUNT: " + mapper.delete(6L));
+	}
+		
+		
+	@Test
+	public void testRead() {
+		BoardVO board = mapper.read(5L);
+		
+		mapper.insertSelectKey(board);
+		
+		log.info(board);
+		
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로작성하는 글 select key");
+		board.setContent("새로 작성하는 내용 select key");
+		board.setWriter("newbie");
+		
+		mapper.insertSelectKey(board);
+		
+		log.info(board);
+		
+	}
+	
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("niwbie");
+		
+		mapper.insert(board);
+		
+		log.info(board);
+		
+	}
+	
+	
+	@Test
+	public void testGetList() {
+		mapper.getList().forEach(board -> log.info(board));
+	}
+*/	
+	
+	
+
+}
