231249aa236c76aeb574899df798388bf2f848dc
tncnsfs@gmail.com <tncnsfs@gmail.com>
2019-02-07 14:33:53 +0900

    Part5, Part 6/ FromTeacher

---
+++ ex00_Spring/Teacher_190201/파트6/java설정관련/RootConfig.java
@@ -0,0 +1,71 @@
+package org.zerock.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@ComponentScan(basePackages= {"org.zerock.service"})
+@ComponentScan(basePackages="org.zerock.aop")
+@ComponentScan(basePackages="org.zerock.task")
+
+@EnableAspectJAutoProxy
+@EnableScheduling
+@EnableTransactionManagement
+
+@MapperScan(basePackages= {"org.zerock.mapper"})
+public class RootConfig {
+
+	
+	
+  @Bean
+  public DataSource dataSource() {
+    HikariConfig hikariConfig = new HikariConfig();
+    hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+    hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:XE");
+
+    hikariConfig.setUsername("book_ex");
+    hikariConfig.setPassword("book_ex");
+
+    hikariConfig.setMinimumIdle(5);
+    // test Query
+    hikariConfig.setConnectionTestQuery("SELECT sysdate FROM dual");
+    hikariConfig.setPoolName("springHikariCP");
+
+    hikariConfig.addDataSourceProperty("dataSource.cachePrepStmts", "true");
+    hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSize", "200");
+    hikariConfig.addDataSourceProperty("dataSource.prepStmtCacheSqlLimit", "2048");
+    hikariConfig.addDataSourceProperty("dataSource.useServerPrepStmts", "true");
+
+    HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+    return dataSource;
+  }
+
+  @Bean
+  public SqlSessionFactory sqlSessionFactory() throws Exception {
+    SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+    sqlSessionFactory.setDataSource(dataSource());
+    return (SqlSessionFactory) sqlSessionFactory.getObject();
+  }
+  
+  @Bean
+  public DataSourceTransactionManager txManager() {
+      return new DataSourceTransactionManager(dataSource());
+  }
+
+}
+
+
---
+++ ex00_Spring/Teacher_190201/파트6/java설정관련/WebConfig.java
@@ -0,0 +1,58 @@
+package org.zerock.config;
+
+import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { RootConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { ServletConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+//	@Override
+//	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+//
+//		registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
+//
+//	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+
+		return new Filter[] { characterEncodingFilter };
+	}
+
+	@Override
+	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+
+		registration.setInitParameter("throwExceptionIfNoHandlerFound", "true");
+
+		MultipartConfigElement multipartConfig = 
+				new MultipartConfigElement("C:\\upload\\temp", 
+						20971520, 
+						41943040,
+						20971520);
+		
+		registration.setMultipartConfig(multipartConfig);
+
+	}
+
+}
